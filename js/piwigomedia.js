f423bab0c7e04f3d9d38fdc0c286afc73bd02f9a
Joao Coutinho <me@joaoubaldo.com>
2014-09-24 11:54:22 +0100

    updated: faster splitEvery filter new: loading animation updated: code is now restricted to authenticated WP users

--- js/piwigomedia.js
+++ js/piwigomedia.js
@@ -1,6 +1,28 @@
 var app = angular.module("PiwigoMediaApp", []);
 
 
+app.filter('splitEvery', function() {
+    var cache = {};
+    var filter = function(arr, size) {
+        if (!arr) { 
+            return; 
+        }
+        var newArr = [];
+        for (var i=0; i<arr.length; i+=size) {
+            newArr.push(arr.slice(i, i+size));
+        }
+        var arrString = JSON.stringify(arr);
+        var fromCache = cache[arrString+size];
+        if (JSON.stringify(fromCache) === JSON.stringify(newArr)) {
+            return fromCache;
+        }
+        cache[arrString+size] = newArr;
+        return newArr;
+    };
+    return filter;
+});
+
+
 app.controller(
     "PiwigoController",
     ["$scope", "$http", "$location",
@@ -9,7 +31,8 @@ app.controller(
         $scope.site = '';
         $scope.page = 0;
         $scope.postId = -1;
-        $scope.basket = [];
+        $scope.basket = {};
+        $scope.basketOrder = [];
         $scope.perPage = 0;
         $scope.messages = [];
 	    $scope.trMap = {};
@@ -17,50 +40,115 @@ app.controller(
 	    $scope.categories = [];
 	    $scope.images = [];
         $scope.imagesOrder = [];
+        $scope.linkTo = 'page';
+        $scope.imageType = 'thumb';
+        $scope.loading = false;
+        
+        $scope.linkToList = {
+            'none': 'Nothing',
+            'page': 'Page',
+            'fullsize': 'Fullsize'
+        };
+        
+        $scope.imageTypeList = {
+            'thumb': 'Thumbnail',
+            'fullsize': 'Fullsize'            
+        };
+        
     
 	    $scope.setup = function() {
+            $scope.loading = true;
 	        $http.get('app.php?a=setup').success(function(data) {
 	            angular.forEach(data.result, function(value, key) {
 	                this[key] = value;
 	            }, $scope);
+	            $scope.loading = false;
 	        });
 	    };
     
 	    $scope.setup();
 	    
-
-        $scope.refreshSite = function() {
+        $scope.addMessage = function(message, type) {
+            $scope.messages.push({'message': message, 'type': type})
         };
         
-        $scope.addMessage = function(message, type) {
+        $scope.removeMessage = function(index) {
+            $scope.messages.splice(index, 1);
+        };
+        
+        $scope.setLinkTo = function(val) {
+            // TODO: validate
+            $scope.linkTo = val;
+        };
+        
+        $scope.setImageType = function(val) {
+            // TODO: validate
+            $scope.imageType = val;
         };
         
         $scope.inBasket = function(image_id) {
-            if ($scope.findInBasket(image_id) == -1)
-                return false;
-            return true;
-        };
-        
-        $scope.findInBasket = function(image_id) {
-            for(i in $scope.basket) {
-                if ($scope.basket[i].id == image_id) {
-                    return i;
-                }            
-            }            
-            return -1;
+            return $scope.basketOrder.indexOf(image_id) != -1
         };
         
         $scope.toFromBasket = function(image_id) {
-            var idx = $scope.findInBasket(image_id);
-            if (idx == -1) {
-                $scope.basket.push($scope.images[image_id]);
+            var idx = $scope.basketOrder.indexOf(image_id);
+            if (idx != -1) {
+                $scope.basketOrder.splice(idx, 1);
+                delete $scope.basket[image_id];
             } else {
-                $scope.basket.splice(idx, 1);
+                $scope.basket[image_id] = $scope.images[image_id];
+                $scope.basketOrder.push(image_id);
             }
-            console.log($scope.basket);
+        };        
+        
+        $scope.insertPost = function() {
+            var target = "_blank";
+            var align = "";
+            
+            angular.forEach($scope.basketOrder, function(value, key) { 
+                var img = $scope.basket[value];
+                var url;
+                var thumbUrl;
+                
+                if ($scope.imageType == 'thumb')
+                    thumbUrl = img.derivatives.thumb.url;
+                else if ($scope.imageType == 'fullsize')
+                    thumbUrl = img.element_url;
+                    
+                if ($scope.linkTo == 'page')
+                    url = img.page_url;
+                else if ($scope.linkTo == 'fullsize')
+                    url = img.element_url;
+                else if ($scope.linkTo == 'none')
+                    url = '';
+                    
+                if (url) {
+                    html = '<a href="'+url+'" target="'+target+'" '+
+                        'class="piwigomedia-single-image">'+
+                            '<img src="'+thumbUrl+'" class="'+align+'" />'+
+                        '</a>';
+                } else {
+                    html = '<a class="piwigomedia-single-image">'+
+                            '<img src="'+thumbUrl+'" class="'+align+'" />'+
+                        '</a>';
+                }
+                    
+                window.parent.tinyMCE.execCommand('mceInsertContent',
+                    false, html);
+                    
+            }, null);
+            
+            $scope.emptyBasket();
         };
         
+        $scope.emptyBasket = function() {
+            $scope.basket = {};
+            $scope.basketOrder = [];
+        };
         
+        $scope.categoryCount = function() {
+            return Object.keys($scope.categories).length;
+        };
         
         $scope.getFullPath = function(category_id) {
             if (!category_id) return;
@@ -74,21 +162,26 @@ app.controller(
             }
             return res;
         };
-        
+
         $scope.imageClick = function(image_id) {
             $scope.toFromBasket(image_id);
         };
-    
+
 	    $scope.$watch("site", function(new_value, old_value) {
 	        if (!$scope.site)
 	            return;
 
+            $scope.loading = true;
+            
             config = {
                 "params": {"__url__": $scope.site, 
                     "format": "json", 
                     "method": "pwg.categories.getList", 
                     "recursive": true}
             };
+
+            $scope.categories = {};
+            $scope.category = -1;
             
             $http.get('query_remote_pwg.php', config).success(
                 function(data) {
@@ -96,22 +189,27 @@ app.controller(
                         var msg = $scope.trMap["Error while reading from"] + " " + $scope.site + ". " +
                             $scope.trMap["Please verify PiwigoMedia\'s configuration and try again."];
                         $scope.addMessage(msg, 'error');
+                        $scope.loading = false;
                         return;
                     }
                     
-                    $scope.categories = {};
-                    $scope.category = -1;
                     angular.forEach(data.result.categories, 
                         function(value, key) {
                             $scope.categories[value.id] = value;
                         }, 
                         $scope);
+                        
+                    $scope.loading = false;
                 }
             );
-            
 	    });
-	    
+
 	    $scope.$watch("category", function(new_value, old_value) {
+            if ($scope.category == -1)
+                return;
+
+            $scope.loading = true;
+            
     	    var config = {
                 "params": {"__url__": $scope.site, 
                     "format": "json", 
@@ -120,27 +218,29 @@ app.controller(
                     "page": $scope.page, 
                     "per_page": $scope.perPage}
             };
-	        
+
+            $scope.images = {};
+            $scope.imagesOrder = [];
+                
             $http.get('query_remote_pwg.php', config).success(
                 function(data) {
                     if ((data == undefined) || data["stat"] != "ok") {
                         var msg = $scope.trMap["Error reading image information, please try again."];
                         $scope.addMessage(msg, 'error');
+                        $scope.loading = false;
                         return;
                     }
-                    $scope.images = {};
-                    $scope.imagesOrder = [];
                     angular.forEach(data.result.images, 
                         function(value, key) {
                             $scope.images[value.id] = value;
                             $scope.imagesOrder.push(value.id);
                         }, 
                         $scope);
+                        
+                    $scope.loading = false;
                 }
             );
 	    });
-	    
-       
         
 
     }]);
--- popup.php
+++ popup.php
@@ -5,8 +5,8 @@
     $error = null;
 
     // Check permissions
-    #if (get_current_user_id() == 0)
-    #    die('no access');
+    if (get_current_user_id() == 0)
+        die('no access');
 
     if (count($sites) < 1)
         $error = 'not_configured';
@@ -32,32 +32,75 @@
     </head>
     
     <body ng-controller="PiwigoController">
-        <h1 class="text-center"><span class="piwigo-text">Piwigo</span><span class="media-text">Media</span></h1>
+        <h1 class="text-center"><span class="piwigo-text">Piwigo</span><span class="media-text">Media</span> <small>for WP</small></h1>
         
-        <div class="btn-group-sm">
-            <button type="button" class="btn btn-default">
-                <select ng-model="site" ng-options="s for s in sites">
+        <div class="form-group">
+            <label class="col-sm-1">Site</label>
+            <div class="col-sm-11">
+                <select class="form-control" ng-model="site" ng-options="s for s in sites">
                 </select>
-            </button>
-        
-            <button type="button" class="btn btn-default">
-                <select ng-model="category" ng-options="c.id as getFullPath(c.id) for (k, c) in categories">
-                </select>
-            </button>
-            
-            <button type="button" class="btn btn-primary" ng-if="basket.length > 0"><span class="badge">{{basket.length}}</span> Insert</button>
-
+            </div>
         </div>
         
-        <div ng-show="images" class="row"> 
-            <div ng-repeat="(id, image) in images" class="col-xs-2 col-xs-2">
-                <a href="#" class="thumbnail" ng-class="{selected: inBasket(id)}">
-                    <img ng-src="{{image.derivatives.thumb.url}}" ng-click="imageClick(id)">
-                </a>
+        <div class="form-group"  ng-show="categoryCount() > 0 && !loading">
+            <label class="col-sm-1">Category</label>
+            <div class="col-sm-11">
+                <select ng-model="category" ng-options="c.id as getFullPath(c.id) for (k, c) in categories" class="form-control">
+                </select>
             </div>
         </div>
 
-       <div class="footer">PiwigoMedia 2014 - <a href="http://b.joaoubaldo.com/" target="_blank"><?php _e('author website', 'piwigomedia') ?></a>, <a href="http://b.joaoubaldo.com/" target="_blank">github</a> </div>
+        <div class="clearfix"></div>
+        
+        <div class="loader" ng-if="loading"><img src="loader.gif"></div>
+        
+        <div class="panel">
+            <p>{{m.message}}</p>
+            <div class="alert pointer" role="alert" ng-repeat="m in messages" ng-class="{'alert-danger': m.type=='error'}" ng-click="removeMessage($index)">
+                <span class="glyphicon glyphicon-remove text-right"></span> {{m.message}}
+            </div>
+        </div>
+
+        <div class="operations-panel">
+            <div class="btn-group" ng-if="basketOrder.length > 0">
+                <!--<button type="button" class="btn btn-default" ng-if="basketOrder.length > 0">Review</button>-->
+                <div class="btn-group">
+                  <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
+                    Image type <span class="caret"></span>
+                  </button>
+                  <ul class="dropdown-menu" role="menu">
+                    <li ng-repeat="(k, v) in imageTypeList" ng-class="{active: k==imageType}"><a href="#" ng-click="setImageType(k)">{{v}}</a></li>
+                  </ul>
+                </div>
+                
+                <div class="btn-group">
+                  <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
+                    Link to <span class="caret"></span>
+                  </button>
+                  <ul class="dropdown-menu" role="menu">
+                      <li ng-repeat="(k, v) in linkToList" ng-class="{active: k==linkTo}"><a href="#" ng-click="setLinkTo(k)">{{v}}</a></li>
+                  </ul>
+                </div>
+                
+                <button type="button" class="btn btn-primary" ng-click="insertPost()"><span class="badge">{{basketOrder.length}}</span> Insert</button>
+           </div>
+        </div>
+        
+        <div class="grid-container">
+            <div ng-show="!loading" class="row" ng-repeat="row in imagesOrder|splitEvery:4">
+                <div class="col-xs-3" ng-repeat="id in row" ng-click="imageClick(id)">
+                    <a href="#" class="thumbnail" ng-class="{selected: inBasket(id)}">
+                        <img ng-src="{{images[id].derivatives.thumb.url}}">
+                    </a>
+                </div>
+            </div>
+        </div>
+        
+       <hr>
+       <p class="text-right italic-text footer">
+        PiwigoMedia <a href="http://b.joaoubaldo.com/" target="_blank">blog</a> | <a href="https://github.com/joaoubaldo/piwigomedia" target="_blank">github</a>
+       </p>
+       
        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
        <script type='text/javascript' src='js/bootstrap.min.js'></script>
     </body>
